30e848c9d43c052510d51d00b9d6c169230ad0ab
Gon'tzalo <112364134+diazgonza17@users.noreply.github.com>
2024-05-04 23:46:08 -0300

    feat: prettier configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'GTPD - Official Site',
   description: 'Discover our latest games, music, and more.',
-}
+};
 
 /*
 Our slogan is currently on vacation. Please leave a message.
@@ -35,15 +35,15 @@ Hola mi nombre es gonza y esto una prueba de texto
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-950`}>
-        <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'/>
+        <div className="absolute right-[11rem] top-[-6rem] h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem]" />
         <div />
         {children}
       </body>
     </html>
-  )
+  );
 }
